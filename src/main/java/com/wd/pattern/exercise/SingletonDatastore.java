60c0e7b6c421d3d98f4ad045b69c79e86b82bb5e
alemlishar <alextopten03@gmail.com>
2021-10-28 12:08:22 +0200

    cartesianLineSegment Controller fix and datastore for Cartesian space

--- src/main/java/com/wd/pattern/exercise/SingletonDatastore.java
+++ src/main/java/com/wd/pattern/exercise/SingletonDatastore.java
@@ -7,9 +7,23 @@ import java.util.concurrent.ConcurrentHashMap;
 public class SingletonDatastore {
 
 	static ConcurrentHashMap<String, List<Point2D>> CartesianSpacePoints = new ConcurrentHashMap<String, List<Point2D>>();
+	private SingletonDatastore singletonDatastore;
 
 	private SingletonDatastore() {
 
 	}
-	
+
+	public SingletonDatastore SingletonDatastore() {
+		if(CartesianSpacePoints == null)
+			synchronized(SingletonDatastore.class) {
+				if(CartesianSpacePoints == null)
+					singletonDatastore = new SingletonDatastore();
+			}
+		return singletonDatastore;
+	}
+
+	public ConcurrentHashMap<String, List<Point2D>> CartesianDatastore() {
+
+		return CartesianSpacePoints;
+	}
 }
